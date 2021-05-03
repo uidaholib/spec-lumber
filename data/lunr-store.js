281163309da1afd3049d5ed00daee33b42097da6
evanwill <evanpeterw@gmail.com>
2021-05-03 15:51:22 -0700

    minimize lunr store

--- data/lunr-store.js
+++ data/lunr-store.js
@@ -4,20 +4,17 @@
 ---
 {%- assign items = site.data[site.metadata] -%}
 {%- assign posts = site.posts -%}
-{% assign fields = site.data.config-search-blog | where_exp: 'f','f.nostore !="true"' -%}
+{%- assign fields = site.data.config-search-blog | where_exp: 'f','f.nostore !="true"' -%}
 {%- assign everything = posts | concat: items -%}
 var store = [
     {%- for item in everything -%}
-    {
-        {% for f in fields %}{% if item[f.field] %}
-        {{ f.field | jsonify }}: {{ item[f.field] | strip_html | truncatewords: 50 | normalize_whitespace | replace: '""','"' | jsonify }},{% endif %}
-        {% endfor %}
+    { {%- for f in fields -%}{%- if item[f.field] -%}{{ f.field | jsonify }}: {{ item[f.field] | strip_html | truncatewords: 50 | normalize_whitespace | replace: '""','"' | jsonify }}, {%- endif -%}{%- endfor -%}
         "title": {{ item['title'] | jsonify }},
-        {% if item['date'] %}"date": {{ item['date'] | truncate: 10,"" | jsonify }},{%- endif -%}
-        {% if item['tags'] %}"tags": {{ item['tags'] | join: ';' | jsonify }},{%- endif -%}
-        {% if item['categories'] %}"categories": {{ item['categories'] | join: ';' | jsonify }},{%- endif -%}
-        {% if item['subject'] %}"subject": {{ item['subject'] | jsonify }},{%- endif -%}
-        "id": {% if item.objectid %}{{ '/collection/items/' | append: item.objectid | append: '.html' | jsonify }}{% else %}{{ item.url | jsonify }}{% endif %}
+        {%- if item['date'] -%}"date": {{ item['date'] | truncate: 10,"" | jsonify }},{%- endif -%}
+        {%- if item['tags'] -%}"tags": {{ item['tags'] | join: ';' | jsonify }},{%- endif -%}
+        {%- if item['categories'] -%}"categories": {{ item['categories'] | join: ';' | jsonify }},{%- endif -%}
+        {%- if item['subject'] -%}"subject": {{ item['subject'] | jsonify }},{%- endif -%}
+        "id": {%- if item.objectid -%}{{ '/collection/items/' | append: item.objectid | append: '.html' | jsonify }}{%- else -%}{{ item.url | jsonify }}{%- endif -%}
     }{%- unless forloop.last -%},{%- endunless -%}
     {%- endfor -%}
 ];
