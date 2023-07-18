49b96749fe8d56ff8d63dc3373f68e70a30f3a38
EvanWill <evanpeterw@gmail.com>
2023-07-18 07:58:35 -0700

    add small and thumb image to harvester post js

--- assets/harvester-post.js
+++ assets/harvester-post.js
@@ -7,6 +7,8 @@ var harvesterPost = {
     link: "{{ post.url | prepend: site.baseurl | prepend: site.url }}",
     title: {{ post.title | jsonify }},
     image: "{% include image/post-image.html %}",
-    excerpt: {{ post.excerpt | strip_html |truncatewords: 50 | jsonify  }},
+    image_small: "{% include image/post-small.html %}",
+    image_thumb: "{% include image/post-thumb.html %}",
+    excerpt: {{ post.excerpt | strip_html | truncatewords: 50 | jsonify }},
     date: "{{ post.date | date: '%Y-%m-%d' }}"
 };
